07a1bc7d080f8bb1c62a5bb0ad44689fd539c0ee
Soltani <family.soltani@gmail.com>
2018-12-21 12:04:44 +0330

    smiple_echo works

--- examples/simple_echo.ts
+++ examples/simple_echo.ts
@@ -1,11 +1,12 @@
 import { config } from 'dotenv'
 import * as Platform from '../index'
-config({ path: '../.env' })
+import { Logger } from '../src/utils/Logger'
+config({ path: '.env' })
 const Bot = Platform.BaleBot
-
+Logger.log(process.env.BALE_TOKEN)
 let bot = new Bot(process.env.BALE_TOKEN)
 
-bot.hears(['name?', 'name', '/name'], (message, responder) => {
+bot.hears(['name?', 'name', '/name', '/name2'], (message, responder) => {
   responder
     .reply('My name is sample bot!')
     .then(res => {
--- src/tests/unit-tests/SensitiveTest.ts
+++
@@ -1,9 +0,0 @@
-import {TextMessageSensitive} from "../../utils/sensitive/TextMessageSensitive";
-var chai = require("chai");
-var expect = chai.expect;
-
-describe('Sensitive', function () {
-    it('Sensitive Constructor', function () {
-        let sen = new TextMessageSensitive(['a', 'b']);
-    });
-});
